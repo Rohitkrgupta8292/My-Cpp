94ec99ed611f16f35960737dbac0f241942562bd
Rohit kr Gupta <86297467+Rohitkrgupta8292@users.noreply.github.com>
2023-05-24 19:33:47 +0530

    Create compare.cpp

---
+++ compare.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespase std;
+
+int main() {
+  int a.b.c;
+  
+  if (a>b)
+  {
+    if (a>c)
+    {
+      cout<<a<<endl;
+    }  
+    else
+    {
+      cout<<c<<end;
+    }
+ else
+ {
+   if (b>c)
+   {
+     cout<<b<<endl;
+   }
+   else
+   {
+     cout<<c<<endl;
+   }
+ }
+    
+ return 0;
+  }  
+    
+    
+    
+ 
